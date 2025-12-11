14e2ea4e126557627211058c978675d4d061e41b
YuzakiKokuban <heibanbaize@gmail.com>
2025-12-11 19:39:22 +0800

    fix: remove unused `try_umount.rs` to clean up compiler warnings

--- src/mount/try_umount.rs
+++
@@ -1,7 +0,0 @@
-#[cfg(any(target_os = "linux", target_os = "android"))]
-pub use crate::utils::send_unmountable;
-
-#[cfg(not(any(target_os = "linux", target_os = "android")))]
-pub fn send_unmountable<P>(_target: P) 
-where P: AsRef<std::path::Path> {
-}
